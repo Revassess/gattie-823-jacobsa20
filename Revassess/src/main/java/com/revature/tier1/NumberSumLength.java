67f972b5a723ff9d2aa062580fcca6c784e21755
Alli Jacobs <jacobsa20@up.edu>
2020-08-28 12:51:15 -0700

    1

--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -3,9 +3,19 @@ package com.revature.tier1;
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
+
+		int count = String.valueOf(num).length();
+
+		long newnum;
+		long finalnum;
+
+		while (num > 0) {
+			newnum = num % 10;
+			newnum = num / 10;
+			//newnum = newnum.pow(num);
+		}
 		
-		
-		
+
 		return false;
 	}
 }
--- Revassess/src/main/java/com/revature/tier1/SumOverArray.java
+++ Revassess/src/main/java/com/revature/tier1/SumOverArray.java
@@ -1,16 +1,27 @@
 package com.revature.tier1;
 
+import java.util.stream.IntStream;
+
 public class SumOverArray {
 
 	public static int IterateAndSum(int[] arr) {
 
-		while (true) {
-			int sum = 0;
-			for (int i : arr) {
-				sum = sum + i;
+//		int sum = 0;
+//		while (true) {
+//
+//			for (int i : arr) {
+//				sum = sum + i;
+//			}
+		if (arr.equals(0)) {
+			return 0;
+		} else {
+			while (true) {
+				int suma = IntStream.of(arr).sum();
+				return suma;
 			}
-			System.out.println(sum);
-			return sum;
+
 		}
+
 	}
+
 }
