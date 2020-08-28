52be186f52551c23bf117049da0d23218734ca87
Alli Jacobs <jacobsa20@up.edu>
2020-08-28 12:01:28 -0700

    tier1 compare strings, pojo, sum of array

--- Revassess/src/main/java/com/revature/tier1/CompareStrings.java
+++ Revassess/src/main/java/com/revature/tier1/CompareStrings.java
@@ -2,7 +2,16 @@ package com.revature.tier1;
 
 public class CompareStrings {
 
-    public static boolean compareStrings(String s1, String s2){
-        return false;
-    }
+	public static boolean compareStrings(String s1, String s2) {
+
+		// make sure s1 and s2 ignore case sensitivity
+
+		System.out.println("Compare " + s1 + " with " + s2 + "=" + s1.equalsIgnoreCase(s2));
+
+		if (s1.equalsIgnoreCase(s2)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
